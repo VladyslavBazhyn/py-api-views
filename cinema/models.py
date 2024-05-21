7ef8e574685bca379d06a38a9fffaea380dcd4ef
Vlad_work <mr.darmstadtium@gmail.com>
2024-05-21 14:10:58 +0200

    write serializers

--- cinema/models.py
+++ cinema/models.py
@@ -1,9 +1,26 @@
 from django.db import models
 
 
+class Actor(models.Model):
+    first_name = models.CharField(max_length=60)
+    last_name = models.CharField(max_length=60)
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+
+
+class CinemaHall:
+    name = models.CharField(max_length=60)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
+    actors = models.ManyToManyField(Actor, related_name="movies_acted_in")
+    genres = models.ManyToManyField(Genre, related_name="movies_of_genre")
     duration = models.IntegerField()
 
     def __str__(self):
