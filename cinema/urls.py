e83497ba60b8a39ca7f7101be6c4747134b27d88
Vladyslav Bazhyn <147983482+VladyslavBazhyn@users.noreply.github.com>
2024-05-22 14:45:32 +0200

    Update urls.py

--- cinema/urls.py
+++ cinema/urls.py
@@ -33,7 +33,11 @@ cinema_hall_detail = CinemaHallViewSet.as_view(
 urlpatterns = [
     path("", include(router.urls)),
     path("cinema_halls/", cinema_hall_list, name="cinema_hall-list"),
-    path("cinema_halls/<int:pk>/", cinema_hall_detail, name="cinema_hall-detail"),
+    path(
+        "cinema_halls/<int:pk>/", 
+        cinema_hall_detail, 
+        name="cinema_hall-detail"
+    ),
     path("actors/", ActorList.as_view(), name="actor-list"),
     path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
     path("genres/", GenreList.as_view(), name="genre-list"),
